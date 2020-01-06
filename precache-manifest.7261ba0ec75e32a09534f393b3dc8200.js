6068189180e1fd39b971dcf91ab2078ea2f6caa6
Vladislav <vladislavmatkovskiy2015@mail.ru>
2020-01-06 17:53:39 +0300

    Updates

---
+++ precache-manifest.7261ba0ec75e32a09534f393b3dc8200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538a28d441e0918169bdd6f9aa86287c",
+    "url": "/news-front/index.html"
+  },
+  {
+    "revision": "43b91956aa803ad6493d",
+    "url": "/news-front/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "af70ba3a350b82f478b1",
+    "url": "/news-front/static/css/main.0d9fc1a4.chunk.css"
+  },
+  {
+    "revision": "43b91956aa803ad6493d",
+    "url": "/news-front/static/js/2.22ad050f.chunk.js"
+  },
+  {
+    "revision": "af70ba3a350b82f478b1",
+    "url": "/news-front/static/js/main.709bae6a.chunk.js"
+  },
+  {
+    "revision": "f2c9744ad6705a8da867",
+    "url": "/news-front/static/js/runtime-main.7b53f644.js"
+  }
+]);
