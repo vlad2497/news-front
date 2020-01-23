1bcb8e9b4d5dcd20bd6ba69e53abd5105914d8a6
vlad <v.matkovskiy@ideas-world.com>
2020-01-23 18:09:48 +0300

    updated design

--- src/components/UI/NewsBlock/index.js
+++ src/components/UI/NewsBlock/index.js
@@ -4,8 +4,8 @@ import "./style.scss";
 export const NewsBlock = ({ title, urlToImage, description }) => {
   return (
     <div className="news-block">
-      <h3 className="news-block__title">{title}</h3>
       <img className="news-block__img" src={urlToImage} alt="news" />
+      <h3 className="news-block__title">{title}</h3>
       <div className="news-block__description">{description}</div>
     </div>
   );
